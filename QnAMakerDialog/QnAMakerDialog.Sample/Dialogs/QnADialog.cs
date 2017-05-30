c2659c774545d8338cb9b2bbfcadc62967776a0b
Daniel Pfeller <dapfe@microsoft.com>
2017-05-30 19:14:48 +0200

    init

---
+++ QnAMakerDialog/QnAMakerDialog.Sample/Dialogs/QnADialog.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace QnAMakerDialog.Sample.Dialogs
+{
+    [Serializable]
+    [QnAMakerService("975b91a189dd4a878543d802c976e94a", "ff127d71-818b-4c6a-8c6a-debd0633fab0")]
+    public class QnADialog : QnAMakerDialog<object>
+    {
+
+
+        /// <summary>
+        /// This is the default handler used if no specific applicable score handlers are found
+        /// </summary>
+        public override async Task DefaultMatchHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            // ProcessResultAndCreateMessageActivity will remove any attachment markup from the results answer
+            // and add any attachments to a new message activity with the message activity text set by default
+            // to the answer property from the result
+            var messageActivity = ProcessResultAndCreateMessageActivity(context, ref result);
+            messageActivity.Text = result.Answer;
+
+            await context.PostAsync(messageActivity);
+
+            //if event present
+            PromptDialog.Confirm(context, Confirmed, "Ich hab eine passendes Event zu dem Thema gefunden: Event XY, am 7.7.2017. Möchten Sie teilnehmen?");
+
+ 
+
+        }
+        public async Task Confirmed(IDialogContext context, IAwaitable<bool> argument)
+        {
+            bool isYes = await argument;
+            if (isYes)
+            {
+                context.Call(new RegisterEventDialog(), this.AfterEventRegister);
+            }
+            else
+            {
+                context.Wait(MessageReceived);
+            }
+        }
+
+        private async Task AfterEventRegister(IDialogContext context, IAwaitable<object> result)
+        {
+            var sucess = await result;
+            if ((bool)sucess)
+            {
+                await context.PostAsync("Sie sind angemeldet!");
+            }
+            else {
+                await context.PostAsync("Anmeldung abgebrochen");
+
+            }
+            await context.PostAsync("Haben Sie weitere Fragen?");
+
+            context.Wait(MessageReceived);
+
+        }
+
+        /// <summary>
+        /// Handler to respond when QnAMakerResult score is a maximum of 50
+        /// </summary>
+        [QnAMakerResponseHandler(50)]
+        public async Task LowScoreHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            var messageActivity = ProcessResultAndCreateMessageActivity(context, ref result);
+            messageActivity.Text = $"Diese Antwort könnte hilfreich sein... {result.Answer}.";
+            await context.PostAsync(messageActivity);
+
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ QnAMakerDialog/QnAMakerDialog.Sample/Dialogs/RegisterEventDialog.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using System.Linq;
+
+namespace QnAMakerDialog.Sample.Dialogs
+{
+    [Serializable]
+    public class RegisterEventDialog : IDialog<object>
+    {
+        private int attempts = 3;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Wenn Sie bereits Kunde sind geben Sie bitte ihre Kundenummer an. Ansonsten geben Sie uns bitte Ihren Namen bekannt");
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+
+            if (message.Text.All(Char.IsLetter))
+            {
+                //assume name entered
+                await context.PostAsync("Bitte geben Sie ihre Email Adresse an");
+                context.Wait(this.EmailMessageReceivedAsync);
+            }
+            else {
+                context.Done(true);
+            }
+
+
+          
+        }
+
+        private async Task EmailMessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            --attempts;
+            var message = await result;
+            if (!message.Text.Contains("@") && attempts > 0)
+            {
+                await context.PostAsync("Bitte eine valide Email Adresse angeben!");
+                context.Wait(this.EmailMessageReceivedAsync);
+
+            }
+            else if (attempts>0){ 
+            context.Done(true);
+            }
+            else
+            {
+                context.Done(false);
+            }
+        }
+    }
+}
